6c48ea5d1fd6cbe5cfd6a383f41a84fe84c22b17
unknown <cnwmb5@cnwmb5d.cnw.co.nz>
2015-07-16 09:44:24 +1200

    Replaced social links with Twitter #cacophony link

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,18 @@
 <section id="footer-container">
 	<div class="row up-button">
 		<div class="small-12 columns">
-			<a href="#first-project" class="smooth-scroll hero-jump button round black"></a>
+			<a href="#top-nav" class="smooth-scroll hero-jump button round black"></a>
 		</div>
 	</div>
 	<footer>
 		<div class="row"> 
-			<div class="small-12 medium-8 columns">
+			<div class="small-12 medium-9 columns">
 				<p>
 					&copy; 2015 The Cacophony Project. All Rights Reserved.
 				</p>
 			</div> 
-			<div class="small-12 medium-4 columns">
-				<img src="/img/twitter.png">
-				<img src="/img/facebook.png">
-				<img src="/img/linkedin.png">
-				<img src="/img/google-plus.png">
+			<div class="small-12 medium-3 columns">
+				<h3 class="hashtag"><a target="_blank" href=" https://twitter.com/intent/tweet?&hashtags=cacophony">#cacophony</a></h3>
 			</div>
 		</div>
   </footer>
